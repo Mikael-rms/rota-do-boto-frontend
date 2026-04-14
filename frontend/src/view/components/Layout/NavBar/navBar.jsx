d1e893c1bdf2933de7a7b7627f273187e260b0b3
Mikael <mikaelc623@gmail.com>
2026-04-14 14:07:14 -0300

    implementação signOut e singIn

--- frontend/src/view/components/Layout/NavBar/navBar.jsx
+++ frontend/src/view/components/Layout/NavBar/navBar.jsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "lucide-react";
+import { useAuth } from "../../../../context/AuthContext.jsx";
+import { signOut } from "firebase/auth";
+import { auth } from "../../../../firebaseConfig.js";
 
 export default function Navbar() {
+  const { user } = useAuth();
+  const handleSignOut = () => {
+    signOut(auth)
+  } 
 
   return (
     <div className="w-full bg-[rgb(21,40,47)] px-6 py-4 flex items-center justify-between">
@@ -21,16 +28,33 @@ export default function Navbar() {
 
 
       <div className="flex items-center gap-4 px-4">
-        
-        <ShoppingCart className="text-black cursor-pointer" size={22} />
 
-        <Link to="/cadastro" className="text-white cursor-pointer text-sm">
-          Criar conta
-        </Link>
+      {user ? (
+        //user logado
+        <>
+            <ShoppingCart className="text-black cursor-pointer" size={22} />
+            j
+            <Link to="/perfil" className="text-white text-sm">
+              Perfil
+            </Link>
 
-        <Link to="/login" className="bg-white text-black px-4 py-1 rounded-full text-sm font-medium transition-transform duration-200 hover:scale-105">
-          Entrar
-        </Link>
+            <button onClick={handleSignOut}
+            className="bg-white text-black px-4 py-1 rounded-full text-sm font-medium">
+              Sair
+            </button>
+        </>
+        ) : (
+          //user nao logado
+          <>
+            <Link to="/cadastro" className="text-white cursor-pointer text-sm">
+              Criar conta
+            </Link>
+
+            <Link to="/login" className="bg-white text-black px-4 py-1 rounded-full text-sm font-medium transition-transform duration-200 hover:scale-105">
+              Entrar
+            </Link>
+          </>
+        )};
       </div>
     </div>
   );
