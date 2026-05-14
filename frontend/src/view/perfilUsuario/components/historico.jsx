c911499542b9a616a9ce55939b98802824230aea
Mikael <mikaelc623@gmail.com>
2026-05-14 16:20:36 -0300

    feat: craindo historico de pedidos

--- frontend/src/view/perfilUsuario/components/historico.jsx
+++ frontend/src/view/perfilUsuario/components/historico.jsx
@@ -3,8 +3,8 @@ import { Ticket } from "lucide-react";
 function Historico() {
   return (
     <div className="text-center py-20 animate-in slide-in-from-bottom-4 duration-500">
-              <Ticket size={48} className="mx-auto text-gray-200 mb-4" />
-              <h2 className="text-gray-400 italic">Você ainda não possui pedidos realizados.</h2>
-            </div>
+      <Ticket size={48} className="mx-auto text-gray-200 mb-4" />
+      <h2 className="text-gray-400 italic">Você ainda não possui pedidos realizados.</h2>
+    </div>
   )
 }export default Historico;
